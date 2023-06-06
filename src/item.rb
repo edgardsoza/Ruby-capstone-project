dd894adf69db7baa5af7401eb7391ec6b67a578a
Kola Kolade <kolakolade@gmail.com>
2023-06-06 15:35:46 +0100

    Update item class to meet requirement

---
+++ src/item.rb
@@ -0,0 +1,24 @@
+class Item
+    attr_accessor :id, :genre, :author, :source, :label, :publish_date, :archived
+    
+    def initialize(id, genre, author, source, label, publish_date)
+      @id = id
+      @genre = genre
+      @author = author
+      @source = source
+      @label = label
+      @publish_date = publish_date
+      @archived = false
+    end
+    
+    def can_be_archived?
+      (Time.now - @publish_date) > (10 * 365 * 24 * 60 * 60)
+    end
+    
+    def move_to_archive
+      if can_be_archived?
+        @archived = true
+      end
+    end
+  end
+  
