4309c00557dbedcea49c4e73bcf8363a3ac62f33
edgardsoza <120115282+edgardsoza@users.noreply.github.com>
2023-06-06 15:12:40 -0600

    Update author_spec

--- game.rb
+++ game.rb
@@ -7,7 +7,7 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played_at)
-    super()
+    super(last_played_at.to_s)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 require_relative '../author'
+require_relative '../game'
 
 describe Author do
   author = Author.new('Jose', 'Perez')
-  context 'Testing the Auth' do
+    context 'Testing the Auth' do
     it 'The initialize method should create a new author with name and last name' do
       expect(author.first_name).to eq 'Jose'
       expect(author.last_name).to eq 'Perez'
@@ -14,19 +15,6 @@ describe Author do
     end
     it 'initializes the items array as empty' do
       expect(author.items).to be_empty
-    end
-  end
-  context 'Testing the add_item method' do
-    let(:author) { Author.new('John', 'Doe') }
-    let(:item) { double('item') }
-    it 'adds the item to the author\'s items' do
-      author.add_item(item)
-      expect(author.items).to include(item)
-    end
-
-    it 'sets the author as the item\'s author' do
-      expect(item).to receive(:author=).with(author)
-      author.add_item(item)
     end
   end
 end
