913cefdb256666b59fd8884581ebf47443a6d1fd
Kola Kolade <kolakolade@gmail.com>
2023-06-06 21:52:42 +0100

    update book.rb initialize method to call super

--- src/items/book.rb
+++ src/items/book.rb
@@ -1,14 +1,10 @@
 require_relative './item'
 
 class Book < Item
-  attr_accessor :publisher, :cover_state
-  
-  def initialize(publisher, cover_state)
-    @publisher = publisher
-    @cover_state = cover_state
-  end  
+  attr_accessor :publisher, :cover_state 
 
-  def initialize(publisher, cover_state)
+  def initialize(id, genre, author, source, label, publish_date, publisher, cover_state)
+    super(id, genre, author, source, label, publish_date)
     @publisher = publisher
     @cover_state = cover_state
   end  
