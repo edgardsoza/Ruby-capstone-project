51a395dff8cefb1c2e058ac5b95b83a530f0259e
Kola Kolade <kolakolade@gmail.com>
2023-06-06 17:18:01 +0100

    update book.rb to add initialize method

--- src/items/book.rb
+++ src/items/book.rb
@@ -3,6 +3,11 @@ require_relative './item'
 class Book < Item
   attr_accessor :publisher, :cover_state
   
+  def initialize(publisher, cover_state)
+    @publisher = publisher
+    @cover_state = cover_state
+  end  
+  
   def can_be_archived?
     super || @cover_state == "bad"
   end
