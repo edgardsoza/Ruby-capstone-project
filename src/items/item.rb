8ba6f00b274b4377221216632ecbec883caa0b05
Kola Kolade <kolakolade@gmail.com>
2023-06-08 20:01:05 +0100

    Add and modified code for item.rb from @edgardsoza

---
+++ src/items/item.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+# item class
+class Item
+  attr_accessor :genre, :author, :label, :publish_date
+  attr_reader :id, :title, :author, :genre, :source, :publish_date, :archived
+  def initialize(id, title, author, genre, source, publish_date)
+    @id = id
+    @title = title
+    @author = author
+    @genre = genre
+    @source = source
+    @publish_date = publish_date
+  end
+  def can_be_archived?
+    @publish_date > 10.years.ago
+  end
+  def move_to_archive
+    @archived = can_be_archived? ? true : false
+  end
+end
+
+
+
+
+
+
