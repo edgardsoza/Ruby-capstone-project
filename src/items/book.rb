a7f00da143c79c6490bf09c34e7d0e9e2d907172
Kola Kolade <kolakolade@gmail.com>
2023-06-08 20:07:12 +0100

    fix linter errors

--- src/items/book.rb
+++ src/items/book.rb
@@ -6,6 +6,7 @@ require_relative './item'
 class Book < Item
   attr_accessor :author, :genre, :source, :publish_date, :publisher, :cover_state
 
+  # rubocop:disable Metrics/ParameterLists
   def initialize(id, title, author, genre, source, publish_date, publisher, cover_state)
     super(id, title)
     @author = author
@@ -15,6 +16,7 @@ class Book < Item
     @publisher = publisher
     @cover_state = cover_state
   end
+  # rubocop:enable Metrics/ParameterLists
 
   def can_be_archived?
     super || @cover_state == 'bad'
--- src/items/item.rb
+++ src/items/item.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
+
 # item class
 class Item
   attr_accessor :genre, :author, :label, :publish_date
-  attr_reader :id, :title, :author, :genre, :source, :publish_date, :archived
+  attr_reader :id, :archived
+
+  # rubocop:disable Metrics/ParameterLists
   def initialize(id, title, author, genre, source, publish_date)
     @id = id
     @title = title
@@ -11,16 +14,13 @@ class Item
     @source = source
     @publish_date = publish_date
   end
+  # rubocop:enable Metrics/ParameterLists
+
   def can_be_archived?
     @publish_date > 10.years.ago
   end
+
   def move_to_archive
     @archived = can_be_archived? ? true : false
   end
 end
-
-
-
-
-
-
